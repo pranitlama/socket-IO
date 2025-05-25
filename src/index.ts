74af694d4908e750a9540a7c2feb3a74ea5e5812
pranitlama <pranitlama211@gmail.com>
2025-05-25 23:19:15 +0545

    feat(room):added rooms and cleared room data when no socket

--- src/index.ts
+++ src/index.ts
@@ -3,65 +3,129 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { join } from "node:path";
 
+type Troom = {
+  current: number;
+  musicQueue: string[];
+  creator: string;
+  playerIdle: boolean;
+};
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    methods: ["GET"],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
-
-let musicQueue: string[] = [];
-let current = 0;
-let playerIdle = false;
 app.use(express.static(join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "public", "index.html"));
 });
 
+const rooms = new Map<string, Troom>();
+let joinedRoom: string;
+
 io.on("connection", (socket) => {
   console.log("user is connected", socket.id);
 
-  socket.emit("sync-data", { musicQueue, current });
+  socket.on("join-room", (data: { roomId: string; userId: string }) => {
+    console.log(data.roomId, "🎈🎈🎈🎈");
+    joinedRoom = data.roomId;
+    if (!data.roomId) return;
+    console.log(`user ${data.userId} has joined the room `);
+    socket.join(data.roomId);
 
-  socket.on("add-data", (ytId) => {
-    if (!musicQueue.includes(ytId)) {
-      musicQueue.push(ytId);
+    socket.emit("room-created", data.roomId);
 
-      if (playerIdle) {
-        current++;
-        playerIdle = false;
+    if (!rooms.has(data.roomId)) {
+      rooms.set(data.roomId, {
+        current: 0,
+        creator: data.userId,
+        musicQueue: [],
+        playerIdle: false,
+      });
+    }
+    const roomData = rooms.get(data.roomId);
+    console.log(rooms);
+    if (roomData) {
+      io.to(data.roomId).emit("sync-data", {
+        musicQueue: roomData.musicQueue,
+        current: roomData.current,
+        creator: roomData.creator,
+      });
+    }
+  });
+
+  socket.on("add-data", (ytId, roomId) => {
+    const roomData = rooms.get(roomId);
+    if (roomData && !roomData.musicQueue.includes(ytId)) {
+      roomData.musicQueue.push(ytId);
+
+      if (roomData.playerIdle) {
+        roomData.current++;
+        roomData.playerIdle = false;
       }
-      io.emit("sync-data", { musicQueue, current });
+      io.to(roomId).emit("sync-data", {
+        musicQueue: roomData.musicQueue,
+        current: roomData.current,
+        creator: roomData.creator,
+      });
     }
   });
 
-  socket.on("next", () => {
-    if (current < musicQueue.length - 1) {
-      current++;
-      io.emit("sync-data", { musicQueue, current });
+  socket.on("next", (roomId, userId) => {
+    const roomData = rooms.get(roomId);
+    if (roomData && roomData.current < roomData.musicQueue.length - 1) {
+      roomData.current++;
+      io.to(roomId).emit("sync-data", {
+        musicQueue: roomData.musicQueue,
+        current: roomData.current,
+        creator: roomData.creator,
+      });
     }
   });
 
-  socket.on("ended", () => {
-    if (current === musicQueue.length - 1) playerIdle = true;
+  socket.on("ended", (roomId) => {
+    const roomData = rooms.get(roomId);
+    if (roomData && roomData.current === roomData.musicQueue.length - 1)
+      roomData.playerIdle = true;
   });
 
-  socket.on("prev", () => {
-    if (current > 0) {
-      current--;
-      io.emit("sync-data", { musicQueue, current });
+  socket.on("prev", (roomId, userId) => {
+    const roomData = rooms.get(roomId);
+    if (roomData && roomData.current > 0) {
+      roomData.current--;
+      io.to(roomId).emit("sync-data", {
+        musicQueue: roomData.musicQueue,
+        current: roomData.current,
+        creator: roomData.creator,
+      });
     }
   });
 
-  socket.on("play-from-index", (index) => {
-    if (index >= 0 && index < musicQueue.length) {
-      current = index;
-      io.emit("sync-data", { musicQueue, current });
+  socket.on("play-from-index", (index, roomId, userId) => {
+    const roomData = rooms.get(roomId);
+    if (roomData && index >= 0 && index < roomData.musicQueue.length) {
+      roomData.current = index;
+
+      io.to(roomId).emit("sync-data", {
+        musicQueue: roomData.musicQueue,
+        current: roomData.current,
+        creator: roomData.creator,
+      });
     }
   });
-  socket.on("disconnect", () => {
-    console.log("user is disconnected");
+  socket.on("disconnect", async () => {
+    if (!joinedRoom) return;
+
+    const socketsInRoom = await io.in(joinedRoom).fetchSockets();
+
+    if (socketsInRoom.length === 0) {
+      rooms.delete(joinedRoom);
+    }
+
+    console.log("user is disconnected", joinedRoom, rooms, socketsInRoom);
   });
 });
 
